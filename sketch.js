74c09c92be5a8456fa2795a7226b7042fdea0c42
SuraviLawande <65304718+SuraviLawande@users.noreply.github.com>
2020-05-22 22:32:04 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,9 +1,40 @@
+var fixedrect, movingrect;
+
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  createCanvas(1200,800);
+  
+  fixedrect=createSprite(200,200,30,60);
+  movingrect=createSprite(400,200,20,90);
+
+  fixedrect.shapeColor="yellow";
+  movingrect.shapeColor="yellow";
+
+  fixedrect.debug=true;
+  movingrect.debug=true;
+
+  //movingrect.velocityX=-3;
+  //fixedrect.velocityX=3;
 }
 
 function draw() {
-  background(255,255,255);  
+  background(0);  
+ 
+  if(isTouching(movingrect,fixedrect)){
+
+   movingrect.shapeColor="pink";
+   fixedrect.shapeColor="pink";
+
+  }
+  else
+  {
+    movingrect.shapeColor="yellow";
+    fixedrect.shapeColor="yellow";
+  }
+  movingrect.x=World.mouseX;
+  movingrect.y=World.mouseY;
+ 
+  
+
   drawSprites();
 }
+
